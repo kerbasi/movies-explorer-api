0bbc682d630a933032b9baff680e32f3e07c46e7
Kirill <kirill.novikov@gmial.com>
2023-07-22 18:31:17 +0300

    fix bd adress

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const routes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT_DEV, DB_URL_DEV } = require('./utils/configuration');
+
+const { PORT = PORT_DEV, DB_URL = DB_URL_DEV } = process.env;
 
 mongoose.connect(DB_URL);
 
