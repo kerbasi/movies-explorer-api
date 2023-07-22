1787883491a3e74cd1511eac75def33fd0682e11
Kirill <kirill.novikov@gmial.com>
2023-07-22 18:29:10 +0300

    tests for deploy

--- app.js
+++ app.js
@@ -13,9 +13,8 @@ const app = express();
 const routes = require('./routes/index');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/error-handler');
-const { PORT_DEV, DB_URL_DEV } = require('./utils/configuration');
 
-const { PORT = PORT_DEV, DB_URL = DB_URL_DEV } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 mongoose.connect(DB_URL);
 
@@ -36,5 +35,4 @@ app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-  console.log(PORT, DB_URL);
 });
