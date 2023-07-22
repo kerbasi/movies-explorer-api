b61080b9f27acd12c555d09df158e8fda197db69
Kirill <kirill.novikov@gmial.com>
2023-07-22 14:08:02 +0300

    Complete users controllers

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,67 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
+
 const NotFoundError = require('../errors/not-found-error');
 const ValidationError = require('../errors/validation-error');
 const TokenError = require('../errors/token-error');
 const DuplicateError = require('../errors/duplicate-error');
 
-const SUCCESS_CREATE_CODE = 201;
+const { SUCCESS_CREATE_CODE } = require('../utils/constants');
 
 const { MODE_ENV, SECRET_KEY } = process.env;
 
-module.exports.createUser = (req, res, next) => {
+const findMe = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (user) {
+        return res.send({
+          email: user.email,
+          name: user.name,
+        });
+      }
+      throw new NotFoundError('Запрашиваемая карточка не найдена');
+    })
+    .catch((err) => {
+      if (err.name === 'CastError') return next(new ValidationError('Некоректно задан id'));
+      return next(err);
+    });
+};
+
+const updateMe = (req, res, next) => {
+  User.findByIdAndUpdate(
+    req.user._id,
+    {
+      email: req.body.email,
+      name: req.body.name,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  )
+    .then((user) => res.send({
+      name: user.email,
+      about: user.name,
+    }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') return next(new ValidationError('Произошла ошибка, введенные данные неверны'));
+      return next(err);
+    });
+};
+
+const createUser = (req, res, next) => {
   const {
-    name, about, avatar, email, password,
+    email, password, name,
   } = req.body;
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
-      name, about, avatar, email, password: hash,
+      email, password: hash, name,
     }))
     .then((user) => res.status(SUCCESS_CREATE_CODE).send({
       email: user.email,
       name: user.name,
-      about: user.about,
-      avatar: user.avatar,
       _id: user._id,
     }))
     .catch((err) => {
@@ -32,106 +71,38 @@ module.exports.createUser = (req, res, next) => {
     });
 };
 
-module.exports.findAllUsers = (req, res, next) => {
-  User.find({})
-    .then((users) => res.send(users))
-    .catch(next);
-};
-
-module.exports.findUser = (req, res, next) => {
-  User.findById(req.params.userId)
-    .then((user) => {
-      if (user) {
-        return res.send({
-          email: user.email,
-          name: user.name,
-          about: user.about,
-          avatar: user.avatar,
-          _id: user._id,
-        });
-      }
-      throw new NotFoundError('Запрашиваемая карточка не найдена');
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') return next(new ValidationError('Некоректно задан id'));
-      return next(err);
-    });
-};
-
-module.exports.findMe = (req, res, next) => {
-  User.findById(req.user._id)
-    .then((user) => {
-      if (user) {
-        return res.send({
-          email: user.email,
-          name: user.name,
-          about: user.about,
-          avatar: user.avatar,
-          _id: user._id,
-        });
-      }
-      throw new NotFoundError('Запрашиваемая карточка не найдена');
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') return next(new ValidationError('Некоректно задан id'));
-      return next(err);
-    });
-};
-
-module.exports.updateUser = (req, res, next) => {
-  User.findByIdAndUpdate(
-    req.user._id,
-    {
-      name: req.body.name,
-      about: req.body.about,
-    },
-    {
-      new: true,
-      runValidators: true,
-    },
-  )
-    .then((user) => res.send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      _id: user._id,
-    }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') return next(new ValidationError('Произошла ошибка, введенные данные неверны'));
-      return next(err);
-    });
-};
-
-module.exports.updateUserAvatar = (req, res, next) => {
-  User.findByIdAndUpdate(
-    req.user._id,
-    {
-      avatar: req.body.avatar,
-    },
-    {
-      new: true,
-      runValidators: true,
-    },
-  )
-    .then((user) => res.send({
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      _id: user._id,
-    }))
-    .catch((err) => {
-      if (err.name === 'ValidationError') return next(new ValidationError('Произошла ошибка, введенные данные неверны'));
-      return next(err);
-    });
-};
-
-module.exports.login = (req, res, next) => {
+const login = (req, res, next) => {
   const { email, password } = req.body;
-
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, MODE_ENV !== 'production' ? 'some-secret-key' : SECRET_KEY, { expiresIn: '7d' });
-      res.send({ token });
+      const token = jwt.sign(
+        { _id: user._id },
+        MODE_ENV !== 'production' ? 'some-secret-key' : SECRET_KEY,
+        { expiresIn: '7d' },
+      );
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+        sameSite: true,
+      });
+      res.send({ message: 'Успешный логин' });
     })
     .catch((err) => next(new TokenError(err.message)));
+};
+
+const logout = (req, res) => {
+  res.cookie('jwt', 'none', {
+    maxAge: 5000,
+    httpOnly: true,
+    sameSite: true,
+  });
+  res.send({ message: 'Успешный логаут' });
+};
+
+module.exports = {
+  findMe,
+  updateMe,
+  createUser,
+  login,
+  logout,
 };
