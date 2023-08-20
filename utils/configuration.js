8e3781877dcd4ac0e12e070261ec8466e4156519
Kirill <kirill.novikov@gmial.com>
2023-08-21 00:28:10 +0300

    Delete commented

--- utils/configuration.js
+++ utils/configuration.js
@@ -1,7 +1,6 @@
 const SECRET_KEY_DEV = 'some-secret-key';
 
 const DB_URL_DEV = 'mongodb://127.0.0.1:27017/bitfilmsdb';
-// const DB_URL_DEV = 'mongodb://localhost:27017/bitfilmsdb';
 
 const PORT_DEV = 3000;
 
